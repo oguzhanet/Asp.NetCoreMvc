70627f142e002f253e184a2d93df9a49cca727b3
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-05-14 14:29:38 +0300

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            var result = new List<Book>()
+            {
+                new Book{Id=1, BookName="Momo",Writer="Michael Ende"},
+                new Book{Id=2, BookName="Değirmen",Writer="Sabahattin Ali"},
+                new Book{Id=3, BookName="Yaşamak",Writer="Yu Hua"},
+                new Book{Id=4, BookName="Bir Noel Hikayesi",Writer="Charles Dickens"}
+            };
+            return View(result);
+        }
+
+
+    }
+}
---
+++ WebApplication2/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public string Writer { get; set; }
+
+    }
+}
