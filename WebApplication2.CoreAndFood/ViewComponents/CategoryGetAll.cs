c1070f6d7b37393acf801f95843b436f9840e33e
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-06-02 19:56:42 +0300

    02.06.2021

---
+++ WebApplication2.CoreAndFood/ViewComponents/CategoryGetAll.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.CoreAndFood.DataAccess.Concrete.EntityFramework;
+
+namespace WebApplication2.CoreAndFood.ViewComponents
+{
+    public class CategoryGetAll:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            EFCategoryDal categoryDal = new EFCategoryDal();
+            var result = categoryDal.GetAll();
+            return View(result);
+        }
+    }
+}
