78d1adf29495e5ea027e6deef34fc3d8ded4eed7
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-06-02 21:05:07 +0300

    02.06.2021

--- WebApplication2.CoreAndFood/Controllers/ShowcaseController.cs
+++ WebApplication2.CoreAndFood/Controllers/ShowcaseController.cs
@@ -4,13 +4,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication2.CoreAndFood.Models.Entity;
 
 namespace WebApplication2.CoreAndFood.Controllers
 {
     public class ShowcaseController : Controller
     {
+
         [AllowAnonymous]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult CategoryDetails()
         {
             return View();
         }
---
+++ WebApplication2.CoreAndFood/ViewComponents/FoodGetAll.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.CoreAndFood.DataAccess.Concrete.EntityFramework;
+
+namespace WebApplication2.CoreAndFood.ViewComponents
+{
+    public class FoodGetAll:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            EfFoodDal foodDal = new EfFoodDal();
+            var result = foodDal.GetAll();
+            return View(result);
+        }
+    }
+}
